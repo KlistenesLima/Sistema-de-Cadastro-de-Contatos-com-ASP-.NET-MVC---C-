4153b098ba2fd256b56e5ca5d8fd2f84622d906d
KlístenesLima <69425705+KlistenesLima@users.noreply.github.com>
2022-03-29 08:50:38 -0300

    first commit

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
