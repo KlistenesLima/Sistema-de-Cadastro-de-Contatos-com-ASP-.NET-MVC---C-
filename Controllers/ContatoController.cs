0a8572de07507b0ece61df5dcaf74a63b0bcf442
Klistenes de lima leite <KlistenesWar2@hotmail.com>
2022-03-30 17:41:30 -0300

    Adicionando os Required

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -49,15 +49,25 @@ namespace ControleDeContatos.Controllers
         [HttpPost]
         public IActionResult Criar(ContatoModel contato)
         {
-            _contatoRepositorio.Adicionar(contato);
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _contatoRepositorio.Adicionar(contato);
+                return RedirectToAction("Index");
+            }
+
+            return View(contato);
         }
 
         [HttpPost]
         public IActionResult Alterar(ContatoModel contato)
         {
-            _contatoRepositorio.Atualizar(contato);
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _contatoRepositorio.Atualizar(contato);
+                return RedirectToAction("Index");
+            }
+
+            return View("Editar", contato);
         }
     }
 }
